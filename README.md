d2c29fff048ed2036e5f17b9782e9c8951fbb60e
longhoney <long.711.sg@gmail.com>
2025-09-10 10:16:56 +0700

    2025-09-10_10h16

--- README.md
+++ README.md
@@ -8,19 +8,20 @@
 - git commit -m"editted readme"  
 - git push
 
-`Code test 2 driver`
-- Xe đi tiến và đi lùi tốt khi bắt đầu ở tốc độ 100% và giảm dần xuống 50%
-- Xe đi tiến tốt khi bắt đầu ở tốc độ 50% và tăng dần
-- Xe đi lùi tốt khi bắt đầu ở tốc độ 75% và tăng dần
-
 `Quy tắc kết nối`
-- Dây cấp nguồn phải to, không được dùng dây bus
+- Dây cấp nguồn phải to, không được dùng jumper wires --> Phải thay toàn bộ dây, vì sức mạnh của mắt xích yếu nhất cũng là sức mạch của toàn bộ dây chuyền
+
+`Quy tắc cài đặt phần mềm`
+- Cài đặt phần mềm vào chung ổ đĩa cài HĐH (thường là ổ C:)
+- Dữ liệu khi dùng phần mềm lưu ở ổ đĩa riêng (ví dụ là ổ D:)
 
 `Cập nhật`
-- Sau khi thay dây nguồn, dùng sang nguồn tổ ong 12V-5A (Lm2596 3A xuống 9V). Code nạp vào Uno vẫn chạy hiệu quả, nhưng có thể do nguồn cấp nên dù ra lệnh chạy 100% nhưng 4 bánh chạy như 50%
-- lm2596 khá nóng, động cơ cũng nóng lên vì quay chậm trong thời gian dài
-- Dùng Pin sạc thì động cơ quay nhanh. Nhưng ở lệnh stop, các động cơ không stop được
-- Xe chạy đúng 1 lúc rồi lại bị lỗi chương trình: xe chạy liên tục không dừng, ban đầu chạy nhanh sau đó chạy chậm
+- Sau khi thay dây nguồn cấp cho driver (jumper wire --> Xh2.54 4P): Xe chạy theo chường trình, nhưng động cơ không dừng hẳn
+- Vì dây cấp nguồn chính cho 2 driver vẫn là jumper wire (tôi giữ vậy để dễ tháo ra mỗi khi nạp code), xe thiếu nguồn nên hoạt động không đúng như chương trình
+- Tôi thay toàn bộ sang xh2.54 4p, xe chạy tốt, cặp bánh sau quay nhanh hơn hẳn cặp bánh truóc, bánh trái trước quay nhanh hơn hẳn bánh phải trước
 
 `Cập nhật tiến độ`
 - https://docs.google.com/document/d/1SmDWLpdE43OGyefFhWPm1k47KTTY6QEGslYMM_aYo_g/edit?usp=sharing
+
+`Chương trình tham khảo`
+- https://github.com/MinhKhanh614/Mecanum-Car/tree/main 
--- examples/Test_Two_I2C-Motor-Driver/Four-Dir_Two_I2C-Motor-Driver/Four-Dir_Two_I2C-Motor-Driver.ino
+++ examples/Test_Two_I2C-Motor-Driver/Four-Dir_Two_I2C-Motor-Driver/Four-Dir_Two_I2C-Motor-Driver.ino
@@ -19,13 +19,20 @@ void setup() {
 
 void loop() {
   bool dirValue = 1;  // Direction (1 or 0)
-  unsigned char speedValueInPercent = 0;  // (0%->100%)
+  unsigned char speedValueInPercent = 100;  // (0%->100%)
   unsigned char speedSlow = 75;  // (0%->100%)
   unsigned char speedNo = 0;  // (0%->100%)
-  /*
+  
+  //Xe dừng lại
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
+  //2 bánh sau
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
+  delay(2000);
+  
   //Xe đi thẳng
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 100; // 100% (Max Speed)  
+  dirValue = 1; // Direction 1  
   myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
   //2 bánh sau
@@ -34,30 +41,32 @@ void loop() {
   delay(5000);
 
   //Xe dừng lại
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 0; // 100% (Max Speed)  
-  myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
   //2 bánh sau
-  myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
   delay(2000);
 
   //Xe đi lùi
   dirValue = 0; // Direction 0
-  speedValueInPercent = 100; // 100% (Max Speed)  
   myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
   //2 bánh sau
   myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
   myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
   delay(5000);
-  */
-
+  
+  //Xe dừng lại
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
+  //2 bánh sau
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
+  delay(2000);
   
   //Xe tiến sang phải
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 100; // 100% (Max Speed)  
+  dirValue = 1; // Direction 1 
   myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedSlow);    // Run motor B max Speed
   //2 bánh sau
@@ -66,19 +75,14 @@ void loop() {
   delay(5000);
 
   //Xe dừng lại
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 0; // 100% (Max Speed)  
-  myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
   //2 bánh sau
-  myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
-  //delay(2000);
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
+  delay(2000);
   
-  /*
   //Xe tiến sang trái
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 100; // 100% (Max Speed)  
   myDriver.writeMA(dirValue, speedSlow);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
   //2 bánh sau
@@ -97,8 +101,7 @@ void loop() {
   delay(2000);
 
   //Xe lùi sang phải
-  dirValue = 0; // Direction 0
-  speedValueInPercent = 100; // 100% (Max Speed)  
+  dirValue = 0; // Direction 0  
   myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedSlow);    // Run motor B max Speed
   //2 bánh sau
@@ -107,18 +110,15 @@ void loop() {
   delay(5000);
 
   //Xe dừng lại
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 0; // 100% (Max Speed)  
-  myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
   //2 bánh sau
-  myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
   delay(2000);
 
   //Xe lùi sang trái
-  dirValue = 0; // Direction 0
-  speedValueInPercent = 100; // 100% (Max Speed)  
+  dirValue = 0; // Direction 0 
   myDriver.writeMA(dirValue, speedSlow);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
   //2 bánh sau
@@ -127,19 +127,16 @@ void loop() {
   delay(5000);
 
   //Xe dừng lại
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 0; // 100% (Max Speed)  
-  myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
   //2 bánh sau
-  myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
   delay(2000);
   
-  
+  /*
   //Xe quay vòng phải
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 100; // 100% (Max Speed)  
+  dirValue = 1; // Direction 1 
   myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
   myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
   //2 bánh sau
@@ -148,13 +145,11 @@ void loop() {
   delay(5000);
 
   //Xe dừng lại
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 0; // 100% (Max Speed)  
-  myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver.writeMB(dirValue, speedNo);    // Run motor B max Speed
   //2 bánh sau
-  myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
+  myDriver2.writeMA(dirValue, speedNo);    // Run motor A max Speed
+  myDriver2.writeMB(dirValue, speedNo);    // Run motor B max Speed
   delay(2000);
 
   //Xe quay vòng trái
@@ -167,14 +162,5 @@ void loop() {
   myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
   delay(5000);
 
-  //Xe dừng lại
-  dirValue = 1; // Direction 1
-  speedValueInPercent = 0; // 100% (Max Speed)  
-  myDriver.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
-  //2 bánh sau
-  myDriver2.writeMA(dirValue, speedValueInPercent);    // Run motor A max Speed
-  myDriver2.writeMB(dirValue, speedValueInPercent);    // Run motor B max Speed
-  delay(2000);
   */
 }
